00b6dbc82a2d24ab568e6af62c3865c1a8ee5a3f
YiyueMaggieMao <myykitty711@gmail.com>
2020-06-22 04:54:49 -0700

    Found cleaner solution for 0023

---
+++ leetcode/0023-Merge-k-sorted-lists-solution.java
@@ -0,0 +1,42 @@
+// Divide and Conquer makes it so much easier!!
+// Author：lin-jian-you-xue
+// Link：https://leetcode-cn.com/problems/merge-k-sorted-lists/solution/luo-ji-fei-chang-jian-dan-de-javati-jie-shi-jian-j/
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        int len = lists.length;
+        if (len == 0) {
+            return null;
+        }    
+        // Merge two LinkedLists symmetrically 
+        while(len>1) {
+            for (int i=0; i<len/2; i++) {
+                lists[i] = mergeTwoLists(lists[i], lists[len-1-i]);
+            }
+            len = (len+1)/2;
+        }
+        return lists[0];
+    }
+    
+    // Method to merge two lists
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(-1);
+        ListNode p = head;
+        while(l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        if (l1 != null) {
+            p.next = l1;
+        } else if (l2 != null) {
+            p.next = l2;
+        }
+        return head.next;
+    }
+}
